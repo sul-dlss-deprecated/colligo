12ba40645255cb432edecc7037474c6b313712d6
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-11-17 17:59:51 +0000

    Colligo Blacklight app creation

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = 'e8fce56ff4dc23229b2027718b363ab02ebf08da5bbe084721719f707c69fa4e12abda5392e7c86162374562503422984d7fe54fabd39a550cdf9a106d3af1a6'
+
