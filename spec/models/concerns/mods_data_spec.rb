752aad1d5d83b68523dab3612d85be7424c73ded
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-11-26 09:26:56 +0000

    Added method to get mods data as ruby objects

--- spec/models/concerns/mods_data_spec.rb
+++ spec/models/concerns/mods_data_spec.rb
@@ -6,7 +6,7 @@ end
 
 describe ModsData do
   include ModsFixtures
-  let(:document) { SolrDocument.new(modsxml: mods_everything) }
+  let(:document) { SolrDocument.new(modsxml: dms_mods_001) }
 
   describe "#mods" do
     it "should be nil if no modsxml" do
@@ -15,6 +15,24 @@ describe ModsData do
 
     it "should be a ModsDisplay" do
       expect(document.mods).to be_a ModsDisplay::HTML
+    end
+  end
+  
+  describe "#mods_raw" do
+    it "should be nil if no modsxml" do
+      expect(SolrDocument.new().mods_raw).to be_nil
+    end
+
+    it "should be a ModsDisplay" do
+      expect(document.mods_raw).to be_a Stanford::Mods::Record
+    end
+    
+    it "should have title" do
+      expect(document.mods_raw.sort_title).to eq("Old English Homilies, mostly by Ælfric")
+    end
+    
+    it "should have alternative title" do
+      expect(document.mods_raw.alternative_titles).to eq(["Homiliae Saxonicae (IV)"])
     end
   end
 
@@ -26,7 +44,7 @@ describe ModsData do
     it "should return prettified mods" do
       expect(document.prettified_mods).to be_a String
       expect(document.prettified_mods).to match /<div class="CodeRay">/
-      expect(document.prettified_mods).to match />A record with everything</
+      expect(document.prettified_mods).to match />Old English Homilies, mostly by Ælfric</
     end
   end
 end
