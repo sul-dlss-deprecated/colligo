218d02683b581d4d7ea99e7b93a067eeedb5dd3e
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-11-28 02:10:43 +0000

    Added model for iiif manifest data

---
+++ app/models/iiif_manifest.rb
@@ -0,0 +1,7 @@
+class IiifManifest
+  include IiifManifestData
+  
+  def initialize(manifest_url=nil)
+    self.manifest_url = manifest_url if manifest_url
+  end
+end
---
+++ spec/fixtures/iiif_manifest_records/iiif_manifest_fixtures.rb
@@ -0,0 +1,18 @@
+# encoding: UTF-8
+module IiifManifestFixtures
+  def manifest_url_001
+    'http://dms-data.stanford.edu/data/manifests/Stanford/kq131cs7229/manifest.json'
+  end
+
+  def manifest_url_002
+    'http://dms-data.stanford.edu/data/manifests/BnF/jr903ng8662/manifest.json'
+  end
+
+  def manifest_url_003
+    'http://dms-data.stanford.edu/data/manifests/Parker/fh878gz0315/manifest.json'
+  end
+
+  def manifest_url_004
+    'http://dms-data.stanford.edu/data/manifests/Parker/ft757ht3699/manifest.json'
+  end
+end
