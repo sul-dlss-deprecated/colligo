46003142589b518d474e6084f1a135de1e5e2628
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-11-29 21:54:12 +0000

    Added and configured capistrano for deployments

---
+++ config/deploy/staging.rb
@@ -0,0 +1,12 @@
+set :deploy_host, ask("Server", 'e.g. hostname with no ".stanford.edu" or server node designator')
+set :bundle_without, %w{sqlite development test}.join(' ')
+set :deploy_to, "/opt/app/#{fetch(:user)}/#{fetch(:application)}"
+
+server_extensions = ['a', 'b', 'c']
+
+server_extensions.each do |extension|
+  server "#{fetch(:deploy_host)}-#{extension}.stanford.edu", user: fetch(:user), roles: %w{web db app}
+end
+
+Capistrano::OneTimeKey.generate_one_time_key!
+set :rails_env, "production"
---
+++ config/deploy/testing.rb
@@ -0,0 +1,12 @@
+set :stage, :testing
+set :repo_url, 'https://github.com/anusharanganathan/colligo.git'
+set :branch, "master"
+set :deploy_host, ask("Server", 'e.g. server.stanford.edu')
+set :linked_files, fetch(:linked_files, []).push('config/blacklight.yml', 'config/jetty.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('db', 'jetty')
+set :keep_releases, 3
+
+server fetch(:deploy_host), user: fetch(:user), roles: %w{web db app}
+
+set :rails_env, :development
+
