5f868a3270ce1897d8f5f66f745a37c682579b0c
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-11-29 20:40:41 +0000

    First pass at customizing the solr options for the app

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -56,20 +56,26 @@ class CatalogController < ApplicationController
     # :show may be set to false if you don't want the facet to be drawn in the 
     # facet bar
     config.add_facet_field 'format', :label => 'Format'
-    config.add_facet_field 'pub_date', :label => 'Publication Year', :single => true
-    config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20 
-    config.add_facet_field 'language_facet', :label => 'Language', :limit => true 
-    config.add_facet_field 'lc_1letter_facet', :label => 'Call Number' 
-    config.add_facet_field 'subject_geo_facet', :label => 'Region' 
-    config.add_facet_field 'subject_era_facet', :label => 'Era'  
+    config.add_facet_field 'type_of_resource_facet', :label => 'Type of resource'
+    config.add_facet_field 'authors_all_facet', :label => 'Authors'
+    config.add_facet_field 'topic_facet', :label => 'Topic', :limit => 20
+    config.add_facet_field 'geographic_facet', :label => 'Region'
+    config.add_facet_field 'era_facet', :label => 'Era'
+    config.add_facet_field 'manuscript_facet', :label => 'Manuscript'
+    config.add_facet_field 'language', :label => 'Language', :limit => true
+    #config.add_facet_field 'pub_year_t', :label => 'Publication Year', :single => true
+    config.add_facet_field 'place_search', :label => 'Place of origin'
+    config.add_facet_field 'model', :label => 'Type'
+    config.add_facet_field 'folio', :label => 'Folio'
+    config.add_facet_field 'collection', :label => 'Repository'
 
-    config.add_facet_field 'example_pivot_field', :label => 'Pivot Field', :pivot => ['format', 'language_facet']
+    config.add_facet_field 'example_pivot_field', :label => 'Pivot Field', :pivot => ['format', 'language']
 
-    config.add_facet_field 'example_query_facet_field', :label => 'Publish Date', :query => {
-       :years_5 => { :label => 'within 5 Years', :fq => "pub_date:[#{Time.now.year - 5 } TO *]" },
-       :years_10 => { :label => 'within 10 Years', :fq => "pub_date:[#{Time.now.year - 10 } TO *]" },
-       :years_25 => { :label => 'within 25 Years', :fq => "pub_date:[#{Time.now.year - 25 } TO *]" }
-    }
+    #config.add_facet_field 'example_query_facet_field', :label => 'Publish Date', :query => {
+    #   :years_5 => { :label => 'within 5 Years', :fq => "pub_year_t:[#{Time.now.year - 5 } TO *]" },
+    #   :years_10 => { :label => 'within 10 Years', :fq => "pub_year_t:[#{Time.now.year - 10 } TO *]" },
+    #   :years_25 => { :label => 'within 25 Years', :fq => "pub_year_t:[#{Time.now.year - 25 } TO *]" }
+    #}
 
 
     # Have BL send all facet field names to Solr, which has been the default
@@ -80,31 +86,24 @@ class CatalogController < ApplicationController
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display 
     config.add_index_field 'title_display', :label => 'Title'
-    config.add_index_field 'title_vern_display', :label => 'Title'
-    config.add_index_field 'author_display', :label => 'Author'
-    config.add_index_field 'author_vern_display', :label => 'Author'
+    config.add_index_field 'authors_all_display', :label => 'Author'
     config.add_index_field 'format', :label => 'Format'
-    config.add_index_field 'language_facet', :label => 'Language'
-    config.add_index_field 'published_display', :label => 'Published'
-    config.add_index_field 'published_vern_display', :label => 'Published'
-    config.add_index_field 'lc_callnum_display', :label => 'Call number'
-
+    config.add_index_field 'language', :label => 'Language'
+    config.add_index_field 'abstract_display', :label => 'Abstract'
+    config.add_index_field 'topic_display', :label => 'Topic'
+    config.add_index_field 'geographic_display', :label => 'Region'
+    config.add_index_field 'era_display', :label => 'Era'
+    config.add_index_field 'collection', :label => 'Repository'
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display 
     config.add_show_field 'title_display', :label => 'Title'
-    config.add_show_field 'title_vern_display', :label => 'Title'
     config.add_show_field 'subtitle_display', :label => 'Subtitle'
-    config.add_show_field 'subtitle_vern_display', :label => 'Subtitle'
-    config.add_show_field 'author_display', :label => 'Author'
-    config.add_show_field 'author_vern_display', :label => 'Author'
+    config.add_show_field 'title_alternate_display', :label => 'Alternate titles'
+    config.add_show_field 'title_other_display', :label => 'Other titles'
+    config.add_show_field 'authors_all_display', :label => 'Author'
     config.add_show_field 'format', :label => 'Format'
-    config.add_show_field 'url_fulltext_display', :label => 'URL'
-    config.add_show_field 'url_suppl_display', :label => 'More Information'
-    config.add_show_field 'language_facet', :label => 'Language'
-    config.add_show_field 'published_display', :label => 'Published'
-    config.add_show_field 'published_vern_display', :label => 'Published'
-    config.add_show_field 'lc_callnum_display', :label => 'Call number'
-    config.add_show_field 'isbn_t', :label => 'ISBN'
+    config.add_show_field 'language', :label => 'Language'
+    config.add_show_field 'collection', :label => 'Repository'
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
@@ -169,10 +168,9 @@ class CatalogController < ApplicationController
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
-    config.add_sort_field 'score desc, pub_date_sort desc, title_sort asc', :label => 'relevance'
-    config.add_sort_field 'pub_date_sort desc, title_sort asc', :label => 'year'
-    config.add_sort_field 'author_sort asc, title_sort asc', :label => 'author'
-    config.add_sort_field 'title_sort asc, pub_date_sort desc', :label => 'title'
+    config.add_sort_field 'score desc, title_sort asc', :label => 'relevance'
+    config.add_sort_field 'authors_all_facet asc, title_sort asc', :label => 'author'
+    config.add_sort_field 'title_sort asc', :label => 'title'
 
     # If there are more than this many search results, no spelling ("did you 
     # mean") suggestion is offered.
