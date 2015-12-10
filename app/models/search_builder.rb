98471eb461c33b7352d3d12f5fc05980e7de8784
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-12-10 16:16:31 +0000

    Added new request handlers to search descriptions, annotations and transcriptions

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,3 +1,16 @@
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
+  
+  def add_manuscript_filter(solr_parameters)
+    solr_parameters[:qt] = 'descriptions'
+  end
+
+  def add_annotation_filter(solr_parameters)
+    solr_parameters[:qt] = 'annotations'
+  end
+
+  def add_transcription_filter(solr_parameters)
+    solr_parameters[:qt] = 'transcriptions'
+  end
+
 end
