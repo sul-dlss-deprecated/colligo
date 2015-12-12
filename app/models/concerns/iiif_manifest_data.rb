699718a849b830b69500cf2da346b8af914e4254
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-12-12 20:24:09 +0000

    Extract manifest thumbnail for manuscript display

--- app/models/concerns/iiif_manifest_data.rb
+++ app/models/concerns/iiif_manifest_data.rb
@@ -2,7 +2,7 @@ module IiifManifestData
   include JsonReader
   extend ActiveSupport::Concern
 
-  attr_accessor :manifest_url, :manifest, :modsxml
+  attr_accessor :manifest_url, :manifest, :modsxml, :thumbnail
 
   def read_manifest
     return nil unless self.manifest_url
@@ -22,6 +22,14 @@ module IiifManifestData
     self.read_manifest unless self.manifest 
     return nil unless self.manifest
     self.manifest["@id"].match( /\/([a-zA-Z0-9]{11})\// ).to_s.gsub('/', '')
+  end
+
+  def thumbnail
+    return nil unless self.manifest_url
+    self.read_manifest unless self.manifest
+    return nil unless self.manifest
+    return nil unless self.manifest.has_key?("thumbnail")
+    self.manifest["thumbnail"]["@id"]
   end
 
   def mods_url
