aa998cab88a1e3dee99afa819ad5ff5f551a29d9
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-12-17 14:34:59 +0000

    Century slider on homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,11 +15,8 @@
 //= require turbolinks//
 // Required by Blacklight
 //= require blacklight/blacklight
-//= require colligo
-//= require browse_century
-
 //= require_tree .
-
+//
 // For blacklight_range_limit built-in JS, if you don't want it you don't need
 // this:
 //= require 'blacklight_range_limit'
