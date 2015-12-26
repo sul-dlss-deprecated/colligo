11b683ba94a77e7a4287a77eb9741976291830d0
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-12-26 14:14:19 +0000

    query to check for facets improved and specs added

--- app/helpers/colligo/render_simple_constraints_helper_behavior.rb
+++ app/helpers/colligo/render_simple_constraints_helper_behavior.rb
@@ -1,16 +1,11 @@
 require 'blacklight/search_state'
 require 'blacklight/parameters'
-# All methods in here are 'api' that may be over-ridden by plugins and local
-# code, so method signatures and semantics should not be changed casually.
-# implementations can be of course.
-#
-# Includes methods for rendering contraints graphically on the
+# Includes methods for rendering constraints graphically on the
 # search results page (render_constraints(_*))
 module Colligo::RenderSimpleConstraintsHelperBehavior
-
   ##
   # Check if the query has any search constraints defined
-  # 
+  #
   # @param [Hash] query parameters
   # @return [Boolean]
   def query_has_search_constraints?(localized_params = params)
@@ -19,37 +14,48 @@ module Colligo::RenderSimpleConstraintsHelperBehavior
 
   ##
   # Check if the query has any facet constraints defined
-  # 
+  #
   # @param [Hash] query parameters
   # @return [Boolean]
   def query_has_facet_constraints?(localized_params = params)
-    !(localized_params[:f].blank?)
+    return false unless localized_params.key?(:f)
+    return false if localized_params[:f].blank?
+    facetvals = {}
+    localized_params[:f].each do |k, v|
+      if v.is_a?(Array)
+        facetvals[k] = v.reject(&:blank?)
+      elsif !v.blank?
+        facetvals[k] = [v]
+      else
+        facetvals[k] = [nil]
+      end
+    end
+    facetvals.any? { |_k, v| !v.blank? && v.any? if v.is_a?(Array) }
   end
-
 
   ##
   # Render the actual constraints, not including header or footer
-  # info. 
-  # 
+  # info.
+  #
   # @param [Hash] query parameters
   # @return [String]
   def render_simple_constraints(localized_params = params)
     render_simple_constraints_query(localized_params) + render_simple_constraints_filters(localized_params)
   end
-  
+
   ##
   # Render the query constraints
-  # 
+  #
   # @param [Hash] query parameters
   # @return [String]
   def render_simple_constraints_query(localized_params = params)
     # So simple don't need a view template, we can just do it here.
     scope = localized_params.delete(:route_set) || self
-    return "".html_safe if localized_params[:q].blank?
+    return ''.html_safe if localized_params[:q].blank?
     render_simple_constraint_element(simple_constraint_query_label(localized_params),
-          localized_params[:q],
-          :classes => ["simple-query query"])
-          #:remove => scope.url_for(localized_params.merge(:q=>nil, :action=>'index')) )
+                                     localized_params[:q],
+                                     classes: ['simple-query query'])
+    #:remove => scope.url_for(localized_params.merge(:q=>nil, :action=>'index')) )
   end
 
   ##
@@ -57,14 +63,14 @@ module Colligo::RenderSimpleConstraintsHelperBehavior
   # @param [Hash] localized_params query parameters
   # @return [String]
   def render_simple_constraints_filters(localized_params = params)
-     return "".html_safe unless localized_params[:f]
-     path = Blacklight::SearchState.new(localized_params, blacklight_config)
-     content = []
-     localized_params[:f].each_pair do |facet,values|
-       content << render_simple_filter_element(facet, values, path)
-     end
+    return ''.html_safe unless localized_params[:f]
+    path = Blacklight::SearchState.new(localized_params, blacklight_config)
+    content = []
+    localized_params[:f].each_pair do |facet, values|
+      content << render_simple_filter_element(facet, values, path)
+    end
 
-     safe_join(content.flatten, "\n")
+    safe_join(content.flatten, "\n")
   end
 
   ##
@@ -78,10 +84,10 @@ module Colligo::RenderSimpleConstraintsHelperBehavior
 
     safe_join(values.map do |val|
       next if val.blank? # skip empty string
-      render_simple_constraint_element( facet_field_label(facet_config.key), facet_display_value(facet, val),
-                  remove: search_action_path(path.remove_facet_params(facet, val)),
-                  classes: ["filter", "simple-filter", "filter-" + facet.parameterize]
-                )
+      render_simple_constraint_element(facet_field_label(facet_config.key), facet_display_value(facet, val),
+                                       remove: search_action_path(path.remove_facet_params(facet, val)),
+                                       classes: ['filter', 'simple-filter', 'filter-' + facet.parameterize]
+                                      )
     end, "\n")
   end
 
@@ -100,11 +106,10 @@ module Colligo::RenderSimpleConstraintsHelperBehavior
   # @option options [Array<String>] :classes an array of classes to add to container span for constraint.
   # @return [String]
   def render_simple_constraint_element(label, value, options = {})
-    render(:partial => "shared/simple_constraints_element", :locals => {:label => label, :value => value, :options => options})    
+    render(partial: 'shared/simple_constraints_element', locals: { label: label, value: value, options: options })
   end
 
   def simple_constraint_query_label(localized_params = params)
     label_for_search_field(localized_params[:search_field])
-  end 
-
+  end
 end
---
+++ spec/helpers/render_constraints_helper_spec.rb
@@ -0,0 +1,102 @@
+require 'spec_helper'
+
+describe RenderConstraintsHelper do
+  let(:config) do
+    Blacklight::Configuration.new do |config|
+      config.add_facet_field 'type'
+    end
+  end
+
+  before do
+    # the helper methods below infer paths from the current route
+    controller.request.path_parameters[:controller] = 'catalog'
+    allow(helper).to receive(:search_action_path) do |*args|
+      root_path *args
+    end
+    allow(helper).to receive(:label_for_search_field).and_return('Search')
+  end
+
+  describe '#query_has_search_constraints?' do
+    let(:ans1) { helper.query_has_search_constraints?(q: '') }
+    let(:ans2) { helper.query_has_search_constraints?(q: 'foobar') }
+    let(:ans3) { helper.query_has_search_constraints?(f: { 'type' => [''] }) }
+    it 'should render false for empty query' do
+      expect(ans1).to be_false
+    end
+    it 'should render true for query' do
+      expect(ans2).to be_true
+    end
+    it 'should render false for no query' do
+      expect(ans3).to be_false
+    end
+  end
+
+  describe '#query_has_facet_constraints?' do
+    let(:ans1) { helper.query_has_facet_constraints?(f: { 'type' => [], 'model' => [], 'collection' => nil }) }
+    let(:ans2) { helper.query_has_facet_constraints?(f: { 'type' => [], 'model' => [''], 'collection' => nil }) }
+    let(:ans3) { helper.query_has_facet_constraints?(f: { 'type' => [''], 'model' => [], 'collection' => '' }) }
+    let(:ans4) { helper.query_has_facet_constraints?(f: { 'type' => [], 'model' => [], 'collection' => 'test' }) }
+    let(:ans5) { helper.query_has_facet_constraints?(f: { 'type' => [''], 'model' => [], 'collection' => 'test' }) }
+    let(:ans6) { helper.query_has_facet_constraints?(f: { 'type' => ['foo'], 'model' => [''], 'collection' => '' }) }
+    let(:ans7) { helper.query_has_facet_constraints?(f: { 'type' => ['foo'], 'model' => ['bar'], 'collection' => 'test' }) }
+    let(:ans8) { helper.query_has_facet_constraints?(q: 'foobar') }
+    it 'should render false for empty facets' do
+      expect(ans1).to be_false
+      expect(ans2).to be_false
+      expect(ans3).to be_false
+    end
+    it 'should render true for facets' do
+      expect(ans4).to be_true
+      expect(ans5).to be_true
+      expect(ans6).to be_true
+      expect(ans7).to be_true
+    end
+    it 'should render false for no facets' do
+      expect(ans8).to be_false
+    end
+  end
+
+  describe '#render_simple_constraints_query' do
+    let(:my_engine) { double('Engine') }
+    it 'should have a link relative to the current url' do
+      expect(helper.render_simple_constraints_query(q: 'foobar')).to have_selector('span.filterValue', text: 'foobar')
+    end
+    it 'should accept an optional route set' do
+      expect(helper.render_simple_constraints_query(q: 'foobar', route_set: my_engine)).to have_selector('span.filterValue', text: 'foobar')
+    end
+  end
+
+  describe '#render_simple_constraints_filters' do
+    before do
+      allow(helper).to receive(:blacklight_config).and_return(config)
+    end
+    let(:subject) { helper.render_simple_constraints_filters(f: { 'type' => [''] }) }
+    let(:selections) { helper.render_simple_constraints_filters(f: { 'geographic_facet' => ['Great Britain', 'Israel'], 'model' => ['Manuscript'] }) }
+    it 'should render nothing for empty facet limit param' do
+      expect(subject).to be_blank
+    end
+    it 'should render facet labels and values' do
+      expect(selections).to have_selector('span.filter-geographic_facet span.filterValue', text: 'Great Britain')
+      expect(selections).to have_selector('span.filter-geographic_facet span.filterName', text: 'Geographic facet')
+      expect(selections).to have_selector('span.filter-geographic_facet span.filterValue', text: 'Israel')
+      expect(selections).to have_selector('span.filter-geographic_facet span.filterName', text: 'Geographic facet')
+      expect(selections).to have_selector('span.filter-model span.filterValue', text: 'Manuscript')
+      expect(selections).to have_selector('span.filter-model span.filterName', text: 'Model')
+    end
+  end
+
+  describe '#render_simple_filter_element' do
+    before do
+      allow(helper).to receive(:blacklight_config).and_return(config)
+      expect(helper).to receive(:facet_field_label).with('type').and_return('Item Type')
+    end
+    subject { helper.render_simple_filter_element('type', ['journal'], path) }
+
+    let(:path) { Blacklight::SearchState.new({ q: 'biz' }, config) }
+
+    it 'should have a link relative to the current url' do
+      # expect(subject).to have_link 'Remove constraint Item Type: journal', href: '/catalog?q=biz'
+      expect(subject).to have_selector '.filterName', text: 'Item Type'
+    end
+  end
+end
