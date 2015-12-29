24eb7099c8c93cda152ce5ff71cfc90ab7a1bc90
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-12-29 04:19:45 +0000

    Place all content within col to align margins

--- spec/views/manuscript/show.html.erb_spec.rb
+++ spec/views/manuscript/show.html.erb_spec.rb
@@ -8,10 +8,10 @@ describe '/manuscript/show.html.erb' do
     stub_template 'manuscript/_folio.html.erb' => 'Folio'
     stub_template 'manuscript/_footer.html.erb' => 'Footer'
     render
-    expect(rendered).to have_css('div div.row', text: 'Header')
-    expect(rendered).to have_css('div div.row', text: 'Manifest')
+    expect(rendered).to have_css('div div.row div.col-md-12', text: 'Header')
+    expect(rendered).to have_css('div div.row div.col-md-12', text: 'Manifest')
     expect(rendered).to have_css('div div.row div.col-md-8', text: 'Description')
     expect(rendered).to have_css('div div.row div.col-md-4', text: 'Folio')
-    expect(rendered).to have_css('div div.row', text: 'Footer')
+    expect(rendered).to have_css('div div.row div.col-md-12', text: 'Footer')
   end
 end
