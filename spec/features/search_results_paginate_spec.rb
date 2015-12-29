ccdac72cbf9c9a24915dc22f043ce449b31651f5
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-12-29 18:34:19 +0000

    Extended pagination tests to test last page, page with few results and page with no results for manuscripts and transcriptions

--- spec/features/search_results_paginate_spec.rb
+++ spec/features/search_results_paginate_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'Search page paginate', type: :feature, js: true do
-  it 'should have the pagination options' do
+  it 'should have the pagination options for manuscripts' do
     visit root_path
     click_link 'Descriptions'
     # sleep 2
@@ -9,6 +9,7 @@ describe 'Search page paginate', type: :feature, js: true do
     fill_in 'q', with: 'gospel'
     click_button 'search'
     # default pagination
+    expect(page).to have_css('h2', text: '62 Manuscripts')
     expect(page).to have_css('div#content div.search-panel', count: 10)
     expect(page).to have_css('div.prev-page', text: nil)
     expect(page).to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
@@ -16,15 +17,150 @@ describe 'Search page paginate', type: :feature, js: true do
     expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: 'results')
     # click on next
     click_link('1 - 10 of')
+    expect(page).to have_css('h2', text: '62 Manuscripts')
+    expect(page).to have_css('div#content div.search-panel', count: 10)
     expect(page).to have_css('div.prev-page a.btn-paginate-prev span.glyphicon-arrow-left')
     expect(page).to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
     expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: '11 - 20 of')
     expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: 'results')
+    # click 20 per page
+    click_link('20')
+    expect(page).to have_css('h2', text: '62 Manuscripts')
+    expect(page).to have_css('div#content div.search-panel', count: 20)
+    expect(page).to have_css('div.prev-page', text: nil)
+    expect(page).to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: '1 - 20 of')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: 'results')
+    # click on next
+    click_link('1 - 20 of')
+    expect(page).to have_css('h2', text: '62 Manuscripts')
+    expect(page).to have_css('div#content div.search-panel', count: 20)
+    expect(page).to have_css('div.prev-page a.btn-paginate-prev span.glyphicon-arrow-left')
+    expect(page).to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: '21 - 40 of')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: 'results')
+    # click on next
+    click_link('21 - 40 of')
+    expect(page).to have_css('h2', text: '62 Manuscripts')
+    expect(page).to have_css('div#content div.search-panel', count: 20)
+    expect(page).to have_css('div.prev-page a.btn-paginate-prev span.glyphicon-arrow-left')
+    expect(page).to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: '41 - 60 of')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: 'results')
+    # click on next
+    click_link('41 - 60 of')
+    expect(page).to have_css('h2', text: '62 Manuscripts')
+    expect(page).to have_css('div#content div.search-panel', count: 2)
+    expect(page).to have_css('div.prev-page a.btn-paginate-prev span.glyphicon-arrow-left')
+    expect(page).not_to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
+    expect(page).to have_css('a.btn-paginate-next span.page_entries', text: '61 - 62 of')
+    expect(page).to have_css('a.btn-paginate-next span.page_entries', text: 'results')
     # click on prev
     click_link('previouspage')
+    expect(page).to have_css('h2', text: '62 Manuscripts')
+    expect(page).to have_css('div#content div.search-panel', count: 20)
+    expect(page).to have_css('div.prev-page a.btn-paginate-prev span.glyphicon-arrow-left')
+    expect(page).to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: '41 - 60 of')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: 'results')
+    # should not have pagination options if total is less than 10
+    fill_in 'q', with: 'israel'
+    click_button 'search'
+    expect(page).to have_css('h2', text: '1 Manuscripts')
+    expect(page).to have_css('div#content div.search-panel', count: 1)
+    expect(page).not_to have_css('div.prev-page')
+    expect(page).not_to have_css('a.btn-paginate-next')
+  end
+  it 'should not have pagination options if no results for manuscripts' do
+    visit root_path
+    click_link 'Descriptions'
+    page.evaluate_script('document.getElementById("main_search_field").value') == 'descriptions'
+    fill_in 'q', with: 'foobar'
+    click_button 'search'
+    expect(page).to have_css('h2', text: '0 Manuscripts')
+    expect(page).not_to have_css('div#content div.search-panel')
+    expect(page).not_to have_css('div.prev-page')
+    expect(page).not_to have_css('a.btn-paginate-next')
+  end
+  it 'should have the pagination options for transcriptions' do
+    visit root_path
+    click_link 'Transcriptions'
+    # sleep 2
+    page.evaluate_script('document.getElementById("main_search_field").value') == 'transcriptions'
+    fill_in 'q', with: 'jesus'
+    click_button 'search'
+    # default pagination
+    expect(page).to have_css('h2', text: '24 Transcriptions')
+    expect(page).to have_css('div#content div.search-panel', count: 10)
     expect(page).to have_css('div.prev-page', text: nil)
     expect(page).to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
     expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: '1 - 10 of')
-    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: 'results')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: '24 results')
+    # click on next
+    click_link('1 - 10 of')
+    expect(page).to have_css('h2', text: '24 Transcriptions')
+    expect(page).to have_css('div#content div.search-panel', count: 10)
+    expect(page).to have_css('div.prev-page a.btn-paginate-prev span.glyphicon-arrow-left')
+    expect(page).to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: '11 - 20 of')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: '24 results')
+    # click on next
+    click_link('11 - 20')
+    expect(page).to have_css('h2', text: '24 Transcriptions')
+    expect(page).to have_css('div#content div.search-panel', count: 4)
+    expect(page).to have_css('div.prev-page a.btn-paginate-prev span.glyphicon-arrow-left')
+    expect(page).not_to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
+    expect(page).to have_css('a.btn-paginate-next span.page_entries', text: '21 - 24 of')
+    expect(page).to have_css('a.btn-paginate-next span.page_entries', text: '24 results')
+    # click on prev
+    click_link('previouspage')
+    expect(page).to have_css('h2', text: '24 Transcriptions')
+    expect(page).to have_css('div#content div.search-panel', count: 10)
+    expect(page).to have_css('div.prev-page a.btn-paginate-prev span.glyphicon-arrow-left')
+    expect(page).to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: '11 - 20 of')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: '24 results')
+    # click on 20
+    click_link('20')
+    expect(page).to have_css('h2', text: '24 Transcriptions')
+    expect(page).to have_css('div#content div.search-panel', count: 20)
+    expect(page).to have_css('div.prev-page', text: nil)
+    expect(page).not_to have_css('div.prev-page a.btn-paginate-prev span.glyphicon-arrow-left')
+    expect(page).to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: '1 - 20 of')
+    expect(page).to have_css('a.btn-paginate-next div span.page_entries', text: '24 results')
+    # click on next
+    click_link('1 - 20')
+    expect(page).to have_css('h2', text: '24 Transcriptions')
+    expect(page).to have_css('div#content div.search-panel', count: 4)
+    expect(page).to have_css('div.prev-page a.btn-paginate-prev span.glyphicon-arrow-left')
+    expect(page).not_to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
+    expect(page).to have_css('a.btn-paginate-next span.page_entries', text: '21 - 24 of')
+    expect(page).to have_css('a.btn-paginate-next span.page_entries', text: '24 results')
+    # click on 50
+    click_link('50')
+    expect(page).to have_css('h2', text: '24 Transcriptions')
+    expect(page).to have_css('div#content div.search-panel', count: 24)
+    expect(page).not_to have_css('div.prev-page')
+    expect(page).not_to have_css('a.btn-paginate-next')
+    # should not have pagination options if total is less than 10
+    fill_in 'q', with: 'israel'
+    click_button 'search'
+    expect(page).to have_css('h2', text: '2 Transcriptions')
+    expect(page).to have_css('div#content div.search-panel', count: 2)
+    expect(page).not_to have_css('div.prev-page')
+    expect(page).not_to have_css('a.btn-paginate-next div span.glyphicon-arrow-right')
+    expect(page).not_to have_css('a.btn-paginate-next')
+  end
+  it 'should not have pagination options if no results for manuscripts' do
+    visit root_path
+    click_link 'Transcriptions'
+    page.evaluate_script('document.getElementById("main_search_field").value') == 'transcriptions'
+    fill_in 'q', with: 'foobar'
+    click_button 'search'
+    expect(page).to have_css('h2', text: '0 Transcriptions')
+    expect(page).not_to have_css('div#content div.search-panel')
+    expect(page).not_to have_css('div.prev-page')
+    expect(page).not_to have_css('a.btn-paginate-next')
   end
 end
