b64baee0cb7c5ba8b2a10ee90d14434d0e7995c1
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2016-01-06 22:30:47 +0000

    Add current page details in sidebar

--- app/assets/javascripts/viewer.js
+++ app/assets/javascripts/viewer.js
@@ -1,13 +1,15 @@
 $(function() {
-    if ($("#viewer").length == 0) {
+    var viewer = $("#viewer");
+    if (viewer.length == 0) {
         return;
     }
+
+    // Initialize the mirador viewer
     var vc = $("#viewer_container");
-    // var display_ratio = 1/(1.618 * 2);
-    var viewer = $("#viewer");
     var manifest_uri = viewer.data("manifest-uri");
     var canvas_id = viewer.data("canvas-id");
     viewer.width(vc.width());
+    // var display_ratio = 1/(1.618 * 2);
     viewer.height(viewer.width() * 0.5);
     Mirador({
         "id": "viewer",
@@ -34,12 +36,27 @@ $(function() {
     });
     viewer.css('position', 'relative');
 
-    //use Mirador event bottomPanelSet.[WindowID] ??
-    // var current_page = $("ul.panel-listing-thumbs li img.thumbnail-image");
-    //$(".panel-listing-thumbs").on("click", '.thumbnail-image', function() {
-    //    console.log('Its changed');
-    //    //var imgele = $(this).clone();
-    //    //imgele.css('class', '');
-    //    //imgele.appendTo( ".folio-img" );
-    //});
+    function handle() {
+        // `e` is the event object passed to handle - don't care about it.
+        var highlighted_pages = $("ul.panel-listing-thumbs li.highlight");
+        var current_page = highlighted_pages.first();
+        if (highlighted_pages.length == 2) {
+            current_page = highlighted_pages.eq(1);
+        }
+        // Folio thumbnail
+        var folio_img = $(".folio-img");
+        var img_ele = current_page.find('img').clone();
+        if ( !img_ele.attr('src') ) {
+          img_ele.attr('src', img_ele.attr('data'));
+        }
+        img_ele.removeClass('thumbnail-image highlight');
+        folio_img.html(img_ele);
+        // Folio title
+        var folio_title = $(".foliotitle");
+        folio_title.html(current_page.find('div').text());
+    }
+
+    // update the current page thumbnail each time the focus changes
+    $.subscribe('focusUpdated', handle);
+
 });
