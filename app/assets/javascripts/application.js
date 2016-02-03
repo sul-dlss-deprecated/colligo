433c7f01100c2f950ca13c88fb2c3436062363fc
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2016-02-03 17:39:51 +0000

    Scale font size dynamically

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,8 @@
 //= require blacklight/blacklight
 // For Mirador viewer
 //= require mirador
+// For scaling fonts
+//= require flowtype
 //= require_tree .
 //
 // For blacklight_range_limit built-in JS, if you don't want it you don't need
--- app/assets/javascripts/folio_viewer.js
+++ app/assets/javascripts/folio_viewer.js
@@ -46,6 +46,10 @@ $(function() {
         text_class = 'annotation_text';
     }
     var delta_left = viewer_text.offset().left - viewer.offset().left;
+    var viewer_text_left = viewer_text.offset().left;
+    var viewer_text_width = viewer_text.outerWidth();
+    var max_text_length = get_max_text_length(text_class);
+
     // var delta_top = viewer_text.offset().top - viewer.offset().top;
 
     var anno_check_count = 0;
@@ -60,15 +64,31 @@ $(function() {
         if (annos.length == 0 &&  anno_check_count < 5) {
             setTimeout(image_rect_updated_handle, 1, event, data);
         } else {
-            //console.log(annos.length);
-            //console.log(texts.length);
             $.publish('disableTooltips.' + win_id);
-            //var current_annos = [];
             var index = 0;
+            var number_of_cols = get_annotation_columns(annos);
+            var col_width = viewer_text_width / number_of_cols;
+            var font_ratio = get_font_ratio(max_text_length, number_of_cols);
             annos.each(function(){
                 var p = $(this).offset();
+                var w = $(this).outerWidth();
+                var h = $(this).outerHeight();
+                // Set the col width to the max if the anno box is less than the max width
+                // If anno box is greater than the max, use that (i.e zoomed in)
+                // Offset the width by height, to account for start of text within the annotation box
+                var screen_w = col_width - p.left - delta_left - (h/2) + viewer_text_left;
                 if (texts.length > index) {
-                    $(texts[index]).offset({top: p.top + 5, left: p.left + delta_left});
+                    $(texts[index]).offset({top: p.top + (h/3), left: p.left + delta_left + (h/2)});
+                    if (w < screen_w) {
+                        $(texts[index]).width(screen_w);
+                    } else {
+                        $(texts[index]).width(w);
+                    }
+                    $(texts[index]).flowtype({
+                        //minFont : 6,
+                        //maxFont : 40
+                        fontRatio : font_ratio
+                    });
                     index = index + 1;
                 }
             });
@@ -78,6 +98,38 @@ $(function() {
 
     function focus_updated_handle() {
         anno_check_count = 0;
+    }
+
+    function get_max_text_length(text_class) {
+        var max_length = 0;
+        $("." + text_class).each(function(){
+            var tn = $(this).text().length;
+            if (tn > max_length) {
+                max_length = tn;
+            }
+        });
+        return max_length;
+    }
+
+    function get_annotation_columns(annos) {
+        var cols = [];
+        annos.each(function(){
+            var l = $(this).offset().left;
+            if (cols.indexOf(l) == -1) {
+                cols.push(l);
+            }
+        });
+        return cols.length;
+    }
+
+    function get_font_ratio(max_text_length, number_of_cols) {
+        var ideal_text_length = 75;
+        var text_length_for_col = ideal_text_length / number_of_cols;
+        if (max_text_length > text_length_for_col) {
+            return 40
+        } else {
+            return 30
+        }
     }
 
 });
