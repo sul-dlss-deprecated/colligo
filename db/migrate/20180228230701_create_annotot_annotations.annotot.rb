ca4a5d5ac12cb28fd8f49a5aaeca8c6998e89e50
Jack Reed <phillipjreed@gmail.com>
2018-02-28 16:08:02 -0700

    add annotot migrations

---
+++ db/migrate/20180228230701_create_annotot_annotations.annotot.rb
@@ -0,0 +1,12 @@
+# This migration comes from annotot (originally 20180215183225)
+class CreateAnnototAnnotations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :annotot_annotations do |t|
+      t.string :uuid, index: true
+      t.string :canvas, index: true
+      t.binary :data
+
+      t.timestamps
+    end
+  end
+end
