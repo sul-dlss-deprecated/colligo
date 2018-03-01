6793ea79ecce286e7b5402e9e24d63a4a28a05d5
Jack Reed <phillipjreed@gmail.com>
2018-02-28 18:13:09 -0700

    add rack-cors for serving cors on annos

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,12 @@ module Colligo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '/annotation*', headers: :any, methods: %i[get post options delete patch put]
+      end
+    end
+
   end
 end
